7cccc5f8734fcb02241b2614d0c9bbc6a34e7371
Seek <solverseek666@gmail.com>
2026-09-19 14:07:03 +0800

    Update main.js
    yes? I used gemini and also simplified yes wtf

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-setInterval(() => {
-    document.getElementById("outputDisplay").innerHTML = `\\[${input.value}\\]`;
-}, 10);
+document.getElementById("input").addEventListener("input", () => {
+  document.getElementById("output").textContent = `\$\$${input.value}\$\$`;
+  if (window.MathJax?.typesetPromise) {
+    MathJax.typesetPromise([document.getElementById("output")]);
+  }
+});
